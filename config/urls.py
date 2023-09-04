1a2ba96168cb00d51ae8984e75562c8f66751b40
mishustik-basket333 <iiiskhakov1990@gmail.com>
2023-09-04 14:45:09 +0300

    Docker ver 3

--- config/urls.py
+++ config/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     path('habits/', include('habits.urls', namespace='habits')),
     path('users/', include('users.urls', namespace='users')),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
